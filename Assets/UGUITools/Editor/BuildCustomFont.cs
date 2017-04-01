4ffe3f45f8e471cd439c70cbe4238e132aa8754a
simonne.xu <min.xu@dena.jp>
2017-04-01 10:44:41 +0800

    修复CustomFont无法保存的问题

--- Assets/UGUITools/Editor/BuildCustomFont.cs
+++ Assets/UGUITools/Editor/BuildCustomFont.cs
@@ -1,23 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEditor;
 using System.IO;
 using System.Xml;
 using System.Collections.Generic;
 
-public class BuildCustomFontWindow: EditorWindow{
-	private string m_fontName = "CustomFont";
+public class BuildCustomFontWindow: EditorWindow{	
+	/// <summary>
+	/// The build path.
+	/// </summary>
 	private const string buildPath = "Assets/Fonts/";
 
-	private Texture2D fontImage;
-	private TextAsset fontInfo;
+	private string m_fontName = "CustomFont";
+	private Texture2D m_fontTex = null;
+	private TextAsset m_fontInfo = null;
+	private Font m_font = null;
 
 	[MenuItem("Tools/Font/BuildCustomFont")]
 	public static void ShowWindow()
 	{
-		EditorWindow.GetWindow(typeof(BuildCustomFontWindow));
+		EditorWindow.GetWindow (typeof(BuildCustomFontWindow));
 	}
 
-	bool BuildCustomFont()
+	protected bool BuildCustomFont()
 	{
 		if (!Directory.Exists (buildPath)) {
 			Directory.CreateDirectory (buildPath);
@@ -25,16 +30,18 @@ public class BuildCustomFontWindow: EditorWindow{
 
 		// create material
 		Material material = new Material (Shader.Find ("UI/Default Font"));
-		material.mainTexture = fontImage;
+		material.mainTexture = m_fontTex;
 		AssetDatabase.CreateAsset (material, buildPath + m_fontName + ".mat");
+		AssetDatabase.Refresh ();
 
 		// create font
-		Font font = new Font();
-		AssetDatabase.CreateAsset (font, buildPath + m_fontName + ".fontsettings");
+		m_font = new Font();
+		AssetDatabase.CreateAsset (m_font, buildPath + m_fontName + ".fontsettings");
+		AssetDatabase.Refresh ();
 
 		// get fnt info
 		XmlDocument xml = new XmlDocument();
-		xml.LoadXml (fontInfo.text);
+		xml.LoadXml (m_fontInfo.text);
 		List<CharacterInfo> chtInfoList = new List<CharacterInfo>();
 		XmlNode node = xml.SelectSingleNode("font/chars");
 		foreach (XmlNode nd in node.ChildNodes)
@@ -48,8 +55,8 @@ public class BuildCustomFontWindow: EditorWindow{
 			int offsetX = int.Parse (xe.GetAttribute ("xoffset"));
 			int offsetY = int.Parse (xe.GetAttribute ("yoffset"));
 
-			float texWidth = fontImage.width;
-			float texHeight = fontImage.width;
+			float texWidth = m_fontTex.width;
+			float texHeight = m_fontTex.width;
 
 			CharacterInfo info = new CharacterInfo();
 			info.index = int.Parse(xe.GetAttribute("id"));
@@ -66,33 +73,37 @@ public class BuildCustomFontWindow: EditorWindow{
 			chtInfoList.Add(info);
 		}
 		// config font
-		font.characterInfo = chtInfoList.ToArray();
-		font.material = material;
+		m_font.characterInfo = chtInfoList.ToArray();
+		m_font.material = material;
 
-	
-//		AssetDatabase.Refresh ();
+		EditorUtility.SetDirty (m_font);
+		AssetDatabase.SaveAssets ();
+		AssetDatabase.Refresh ();
+
+		AssetDatabase.LoadAssetAtPath<Font> (buildPath + m_fontName + ".fontsettings");
+
+
 		return true;
 	}
-
 		
-	void OnGUI()
+	protected void OnGUI()
 	{
-		fontImage = (Texture2D) EditorGUILayout.ObjectField("Font Image", fontImage, typeof(Texture2D));
-		fontInfo = (TextAsset) EditorGUILayout.ObjectField("Font Info", fontInfo, typeof(TextAsset));
+		m_fontTex = (Texture2D) EditorGUILayout.ObjectField("Font Image", m_fontTex, typeof(Texture2D));
+		m_fontInfo = (TextAsset) EditorGUILayout.ObjectField("Font Info", m_fontInfo, typeof(TextAsset));
 		m_fontName = EditorGUILayout.TextField ("Font Name", m_fontName);
 
-		if (fontImage != null && fontInfo != null) {
+		if (m_fontTex != null && m_fontInfo != null) {
 			if (GUILayout.Button ("Build")) {
 				if (BuildCustomFont ()) {
-					Debug.Log ("Buil Successful");
+					Debug.Log ("BuildCustomFont >> Buil Successful");
 					this.Close ();
 				}
 			}
 		} else {
-			if (fontImage == null) {
-				GUILayout.Label ("Please Select Font Image");
+			if (m_fontTex == null) {
+				GUILayout.Label ("Please Select Font Texture");
 			} else {
-				GUILayout.Label ("Please Select Font Info");
+				GUILayout.Label ("Please Select Font Info(fnt file)");
 			}
 		}
 	}
